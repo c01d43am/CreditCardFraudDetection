c611f46d30fa81fc49788447cd1f3a769e48e489
A Pawan <57105127+c01d43am@users.noreply.github.com>
2024-01-09 09:56:09 +0530

    Update FraudDetection.py

--- FraudDetection.py
+++ FraudDetection.py
@@ -1,45 +1,43 @@
-# import the required packages
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sys
-import scipy
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
 
-# load the dataset using pandas
-data = pd.read_csv(r'LOCATION OF THE FILE')
+# Load the dataset using pandas
+data = pd.read_csv('LOCATION OF THE FILE')
 
-# dataset exploring
+# Dataset exploring
 print(data.columns)
 
 # Print the shape of the data
-data = data.sample(frac=0.1, random_state = 1)
+data = data.sample(frac=0.1, random_state=1)
 print(data.shape)
 print(data.describe())
 
-# Plot histograms of each parameter 
-data.hist(figsize = (20, 20))
+# Plot histograms of each parameter
+data.hist(figsize=(20, 20))
 plt.show()
 
-# Determine number of fraud cases in dataset
+# Determine the number of fraud cases in the dataset
+fraud = data[data['Class'] == 1]
+valid = data[data['Class'] == 0]
 
-Fraud = data[data['Class'] == 1]
-Valid = data[data['Class'] == 0]
-
-outlier_fraction = len(Fraud)/float(len(Valid))
+outlier_fraction = len(fraud) / float(len(valid))
 print(outlier_fraction)
 
-print('Fraud Cases: {}'.format(len(data[data['Class'] == 1])))
-print('Valid Transactions: {}'.format(len(data[data['Class'] == 0])))
+print('Fraud Cases: {}'.format(len(fraud)))
+print('Valid Transactions: {}'.format(len(valid)))
 
 # Correlation matrix
 corrmat = data.corr()
-fig = plt.figure(figsize = (12, 9))
-
-sns.heatmap(corrmat, vmax = .8, square = True)
+fig = plt.figure(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True)
 plt.show()
 
-# Get all the columns from the dataFrame
+# Get all the columns from the DataFrame
 columns = data.columns.tolist()
 
 # Filter the columns to remove data we do not want
@@ -55,25 +53,19 @@ Y = data[target]
 print(X.shape)
 print(Y.shape)
 
-from sklearn.metrics import classification_report, accuracy_score
-from sklearn.ensemble import IsolationForest
-from sklearn.neighbors import LocalOutlierFactor
-
-# define random states
+# Define random states
 state = 1
 
-# define outlier detection tools to be compared
+# Define outlier detection tools to be compared
 classifiers = {
-    "Isolation Forest": IsolationForest(max_samples=len(X),contamination=outlier_fraction,random_state=state),
-    "Local Outlier Factor": LocalOutlierFactor(n_neighbors=20,contamination=outlier_fraction)}
-    
+    "Isolation Forest": IsolationForest(max_samples=len(X), contamination=outlier_fraction, random_state=state),
+    "Local Outlier Factor": LocalOutlierFactor(n_neighbors=20, contamination=outlier_fraction)}
+
 plt.figure(figsize=(9, 7))
-n_outliers = len(Fraud)
+n_outliers = len(fraud)
 
-
-for i, (clf_name, clf) in enumerate(classifiers.items()):
-    
-    # fit the data and tag outliers
+for clf_name, clf in classifiers.items():
+    # Fit the data and tag outliers
     if clf_name == "Local Outlier Factor":
         y_pred = clf.fit_predict(X)
         scores_pred = clf.negative_outlier_factor_
@@ -81,11 +73,11 @@ for i, (clf_name, clf) in enumerate(classifiers.items()):
         clf.fit(X)
         scores_pred = clf.decision_function(X)
         y_pred = clf.predict(X)
-    
-    # Reshape the prediction values to 0 for valid, 1 for fraud. 
+
+    # Reshape the prediction values to 0 for valid, 1 for fraud.
     y_pred[y_pred == 1] = 0
     y_pred[y_pred == -1] = 1
-    
+
     n_errors = (y_pred != Y).sum()
 
     # Run classification metrics
